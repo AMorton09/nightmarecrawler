4e196c3b2d2ac54f6f41fe7498ffe44625f1d652
Andrew Morton <morton09@me.com>
2017-09-18 14:50:49 -0500

    $ not defined

--- app.js
+++ app.js
@@ -2,13 +2,41 @@
  * Created by 1337 on 9/13/2017.
  */
 const Nightmare = require('nightmare');
+const nightmare = Nightmare();
 const express = require('express');
-const hbs = require('express-handlebars');
 const app     = express();
+const hbs = require('express-handlebars');
 const jquery = require('jquery');
+
 
 app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+
+
+let scrape = (url) => {
+    console.log("2...");
+    nightmare.goto(url)
+        .wait(2000)
+        .evaluate(()=>{
+            console.log("3...");
+            let name = "";
+            $('.prof-ident-name').each(()=>{
+                console.log("4...");
+                name = $(this).text();
+                console.log(name);
+            })
+            console.log("5...");
+    }).catch((err)=>{
+        console.log(err);
+    })
+}
+
+
+app.get("/", (req, res) => {
+    console.log("1...");
+    scrape('http://www.hudl.com/profile/2102198/darie-johnson');
+});
 
 
 app.listen('3000');
