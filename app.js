535bca42279c2364fd81cd23398068af86dc1ec3
Andrew Morton <morton09@me.com>
2017-09-18 14:16:14 -0500

    added jquery

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 /**
  * Created by 1337 on 9/13/2017.
  */
+const Nightmare = requires('nightmare');
+const express = requires('express');
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+const hbs = require('express-handlebars');
+const app     = express();
+const jquery = require('jquery');
+
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
